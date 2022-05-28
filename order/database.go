1aa5955db6cfc9d408c6728497054aafc1223987
Florentin <arsene.florentin.ichb@gmail.com>
2022-05-28 11:18:44 +0200

    Add simple code structure

--- order/database.go
+++ order/database.go
@@ -9,27 +9,24 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Please change this constants according to your setup
 const (
 	host     = "localhost"
 	port     = 5432
 	user     = "postgres"
-	password = "YOUR_PASSWORD"
-	dbname   = "YOUR_DATABASE_NAME"
+	password = "root"
+	dbname   = "postgres"
 )
 
-func main() {
+func openPsqlConnection() string {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		panic(err)
+		return "connection open"
 	}
 	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+	return "connection failed"
 }
