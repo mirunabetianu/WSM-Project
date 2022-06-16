a6bdc81ef0ea321de303d5a17d539a7f7e3384f9
Florentin <arsene.florentin.ichb@gmail.com>
2022-06-16 23:08:51 +0200

    Add k8s connection between payment pods and postgresql

--- payment/database.go
+++ payment/database.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-
+	"os"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 // Please change this constants according to your setup
-const (
+var (
 	host     = "localhost"
 	port     = 5432
 	user     = "postgres"
@@ -16,10 +16,27 @@ const (
 	dbname   = "postgres"
 )
 
-var databaseInfo = fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 var Database *gorm.DB
 
 func connect() (error, *gorm.DB) {
+	if(getEnv("POSTGRES_DB") != "") {
+		dbname = getEnv("POSTGRES_DB")
+	}
+	
+	if (getEnv("POSTGRES_USER") != "") {
+		user = getEnv("POSTGRES_USER")
+	} 
+	
+	if (getEnv("POSTGRES_PASSWORD") != "") {
+		password = getEnv("POSTGRES_PASSWORD")
+	}
+	
+	if (getEnv("POSTGRES_SERVICE_HOST") != "") {
+		host = getEnv("POSTGRES_SERVICE_HOST")
+	}
+
+	var databaseInfo = fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
 	var err error
 	Database, err = gorm.Open(postgres.Open(databaseInfo), &gorm.Config{})
 	if err != nil {
@@ -30,4 +47,13 @@ func connect() (error, *gorm.DB) {
 	Database.AutoMigrate(&Payment{})
 
 	return err, Database
+}
+
+func getEnv(key string) string{
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return ""
+	} else {
+		return val
+	}
 }
