dff2ca7ff5cee7460b6eb8bbd490442bc8c99744
Florentin <arsene.florentin.ichb@gmail.com>
2022-06-17 09:09:44 +0200

    edit sh script

--- deploy-charts-cluster.sh
+++ deploy-charts-cluster.sh
@@ -4,6 +4,3 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
 helm repo update
 
-helm install -f helm-config/nginx-helm-values.yaml nginx ingress-nginx/ingress-nginx
-
-# Add Postgres and MQTT broker
